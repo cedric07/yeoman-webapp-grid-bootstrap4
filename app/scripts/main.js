d59359df0402424cf07fa61f087b423b87cc519c
Cédric Andrietti <cedric.andrietti@gmail.com>
2019-04-15 02:58:53 +0200

    Add Sass files

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,42 @@
-console.log('\'Allo \'Allo!');
+var APP = APP || {};
 
+APP.pref = {
+	screenLarge: 1200,
+	screenMedium: 992,
+	screenSmall: 768,
+	screenExtraSmall: 576
+};
+
+$(document).ready(function () {
+	APP.global.init();
+	APP.global.onResize();
+	APP.global.onScroll();
+});
+
+APP.global = {
+	init: function () {  /** init function */
+  	APP.exemple.init();
+	},
+	onResize: function () {
+		/** onResize function */
+		$(window).on('resize', function () {
+
+		});
+	},
+	onScroll: function () {
+		/** Scroll function */
+		$(window).on('scroll', function () {
+
+		});
+	}
+};
+
+APP.exemple = {
+  exempleVar: $(),
+
+  init: function() {
+    this.exempleVar = $('.myClass');
+
+    APP.exemple.exempleVar.hide();
+  }
+};
